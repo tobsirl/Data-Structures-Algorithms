1614434e29ef60d27452ffa9396fd0c481cfa5f3
Paul Tobin <tobster111@gmail.com>
2019-02-26 10:43:55 +0000

    Creating a Stack using an array + added testing using mocha + chai

---
+++ src/index.js
@@ -0,0 +1,2 @@
+export { default as StackArray } from './data-structures/stack-array';
+export { default as Stack } from './data-structures/stack';
---
+++ test/data-structures/stack-array.spec.js
@@ -0,0 +1,28 @@
+import 'mocha';
+import { expect } from 'chai';
+
+import StackArray from '../../src/data-structures/stack-array';
+
+describe('StackArray', () => {
+  let stack;
+
+  beforeEach(() => {
+    stack = new StackArray();
+  });
+
+  it('starts empty', () => {
+    expect(stack.size()).to.equal(0);
+    expect(stack.isEmpty()).to.equal(true);
+  });
+
+  it('pushes elements', () => {
+    stack.push(1);
+    expect(stack.size()).to.equal(1);
+    stack.push(2);
+    expect(stack.size()).to.equal(2);
+    stack.push(3);
+    expect(stack.size()).to.equal(3);
+
+    expect(stack.isEmpty()).to.equal(false);
+  });
+});
