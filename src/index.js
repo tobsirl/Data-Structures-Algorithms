ae7ebc3333b9391695149b4820205c6a244de77f
Paul Tobin <tobster111@gmail.com>
2019-02-28 17:42:55 +0000

    Added Hot Potato for a circular queue

--- src/index.js
+++ src/index.js
@@ -1,2 +1,14 @@
+// chapter 03
 export { default as StackArray } from './data-structures/stack-array';
 export { default as Stack } from './data-structures/stack';
+// export { hanoi } from './others/hanoi';
+// export { hanoiStack } from './others/hanoi';
+export { baseConverter } from './others/base-converter';
+export { decimalToBinary } from './others/base-converter';
+// export { parenthesesChecker } from './others/balanced-symbols';
+
+// chapter 04
+export { default as Queue } from './data-structures/queue';
+export { default as Deque } from './data-structures/deque';
+export { hotPotato } from './others/hot-potato';
+// export { palindromeChecker } from './others/palindrome-checker';
