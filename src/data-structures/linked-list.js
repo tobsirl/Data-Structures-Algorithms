41f8db9725f39204172a0622a19d0e6d0b1b54d6
Paul Tobin <tobster111@gmail.com>
2019-03-02 11:47:08 +0000

    Implemented a singly linked list

---
+++ src/data-structures/linked-list.js
@@ -0,0 +1,136 @@
+import { defaultEquals } from '../util';
+import { Node } from './models/linked-list-models';
+
+//  export class Node {
+//  constructor(element, next) {
+//    this.element = element;
+//    this.next = next;
+//  }
+// }
+
+export default class LinkedList {
+  constructor(equalsFn = defaultEquals) {
+    this.equalsFn = equalsFn;
+    this.count = 0;
+    this.head = undefined;
+  }
+
+  // This method adds a new element to the end of the list.
+  push(element) {
+    const node = new Node(element); // Create a new Node and pass element to it
+    let current;
+    if (this.head == null) {
+      // Check if the linked list is empty
+      // catches null && undefined
+      this.head = node;
+    } else {
+      current = this.head; // Reference to the first element
+      while (current.next != null) {
+        // loop through the list to get to the last one
+        current = current.next; // last element found when current = undefined or null
+      }
+      current.next = node; // assign next to new element to make the link
+    }
+    this.count++; // increment the size of the list
+  }
+
+  // This method returns the element of a specific position in the list.
+  // If the element does not exist in the list, it returns undefined.
+  getElementAt(index) {
+    if (index >= 0 && index <= this.count) {
+      let node = this.head;
+      for (let i = 0; i < index && node != null; i++) {
+        node = node.next;
+      }
+      return node;
+    }
+    return undefined;
+  }
+
+  // Removes a specific element from the linked list
+  removeAt(index) {
+    // check for out-of-bounds values
+    if (index >= 0 && index <= this.count) {
+      let current = this.head;
+
+      // removing the first item
+      if (index === 0) {
+        this.head = current.head;
+      } else {
+        const previous = this.getElementAt(index - 1);
+        current = previous.next;
+        previous.next = current.next; // link previous with current's next: skip it to remove
+      }
+      this.count--;
+      return current.element;
+    }
+    return undefined;
+  }
+
+  // Removes the first element from the linked list
+  remove(element) {
+    const index = this.indexOf(element);
+    return this.removeAt(index);
+  }
+
+  // Insert an element at a specific position
+  insert(element, index) {
+    // check for out of bounds values
+    if (index >= 0 && index <= this.count) {
+      const node = new Node(element); // Create a new node
+      if (index === 0) {
+        const current = this.head; // attach the node to the linked list.
+        node.next = current; // pointer of the new node to the current
+        this.head = node; // assign the value to the new node
+      } else {
+        const previous = this.getElementAt(index - 1); // find the position for insertion, one position before
+        node.next = previous.next;
+        previous.next = node;
+      }
+      this.count++;
+      return true;
+    }
+    return false;
+  }
+
+  indexOf(element) {
+    let current = this.head;
+    for (let i = 0; i < this.size() && current != null; i++) {
+      if (this.equalsFn(element, current.element)) {
+        return i;
+      }
+      current = current.next;
+    }
+    return -1;
+  }
+
+  isEmpty() {
+    return this.size() === 0;
+  }
+
+  size() {
+    return this.count;
+  }
+
+  getHead() {
+    return this.head;
+  }
+
+  clear() {
+    this.head = undefined;
+    this.count = 0;
+  }
+
+  toString() {
+    if (this.head == null) {
+      return '';
+    }
+    let objString = `${this.head.element}`;
+    let current = this.head.next;
+    for (let i = 1; i < this.size() && current != null; i++) {
+      objString = `${objString},${current.element}`;
+      current = current.next;
+    }
+    return objString;
+  }
+}
