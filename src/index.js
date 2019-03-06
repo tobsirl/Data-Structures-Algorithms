1e1140ce9b79a748ca17713e4ba4a250e78b7e79
Paul Tobin <tobster111@gmail.com>
2019-03-06 10:13:38 +0000

    Implemented a sorted linked list

--- src/index.js
+++ src/index.js
@@ -12,3 +12,18 @@ export { default as Queue } from './data-structures/queue';
 export { default as Deque } from './data-structures/deque';
 export { hotPotato } from './others/hot-potato';
 // export { palindromeChecker } from './others/palindrome-checker';
+
+// chapter 05
+export { default as LinkedList } from './data-structures/linked-list';
+export {
+  default as DoublyLinkedList
+} from './data-structures/doubly-linked-list';
+export {
+  default as CircularLinkedList
+} from './data-structures/circular-linked-list';
+export {
+  default as SortedLinkedList
+} from './data-structures/sorted-linked-list';
+export {
+  default as StackLinkedList
+} from './data-structures/stack-linked-list';
