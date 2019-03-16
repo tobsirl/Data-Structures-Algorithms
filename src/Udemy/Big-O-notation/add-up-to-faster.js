547a9b83ed050a0a868c82271098b4dc223d74ff
Paul Tobin <tobster111@gmail.com>
2019-03-16 13:53:46 +0000

    Added caesar-cipher to questions section

--- src/Udemy/Big-O-notation/add-up-to-faster.js
+++ src/Udemy/Big-O-notation/add-up-to-faster.js
@@ -2,7 +2,7 @@ function addUpTo(n) {
   return (n * (n + 1)) / 2;
 }
 
-let time1 = performance.now();
+const time1 = performance.now();
 addUpTo(1000000000);
-let time2 = performance.now();
+const time2 = performance.now();
 console.log(`Time Elapsed: ${(time2 - time1) / 1000} seconds.`);
--- src/Udemy/Big-O-notation/add-up-to-slower.js
+++ src/Udemy/Big-O-notation/add-up-to-slower.js
@@ -6,7 +6,7 @@ function addUpTo(n) {
   return total;
 }
 
-let t1 = performance.now();
+const t1 = performance.now();
 addUpTo(1000000000);
-let t2 = performance.now();
+const t2 = performance.now();
 console.log(`Time Elapsed: ${(t2 - t1) / 1000} seconds.`);
