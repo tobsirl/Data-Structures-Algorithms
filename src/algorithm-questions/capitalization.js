c3a9c772edd4022c70e268bef61ea032912821d3
Paul Tobin <tobster111@gmail.com>
2019-03-16 13:21:14 +0000

    Added a section on Big O Notation

--- src/algorithm-questions/capitalization.js
+++ src/algorithm-questions/capitalization.js
@@ -1,7 +1,9 @@
 const capitalize = phrase => {
   const words = [];
 
-  for (const word of phrase.split(' ')) { words.push(word[0].toUpperCase() + word.slice(1)); }
+  for (const word of phrase.split(' ')) {
+    words.push(word[0].toUpperCase() + word.slice(1));
+  }
 
   return words.join(' ');
 };
@@ -9,7 +11,9 @@ const capitalize = phrase => {
 const _capitalize = phrase => {
   let title = phrase[0].toUpperCase();
 
-  for (let i = 1; i < phrase.length; i++) { title += phrase[i - 1] === ' ' ? phrase[i].toUpperCase() : phrase[i]; }
+  for (let i = 1; i < phrase.length; i++) {
+    title += phrase[i - 1] === ' ' ? phrase[i].toUpperCase() : phrase[i];
+  }
 
   return title;
 };
