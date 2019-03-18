7a00898af89db10a35e18c09562602dc057cf9b4
Paul Tobin <tobster111@gmail.com>
2019-03-18 19:18:49 +0000

    Added solution to same problem using objects

--- src/Udemy/same.js
+++ src/Udemy/same.js
@@ -24,3 +24,29 @@ function same(arr1, arr2) {
   }
   return true;
 }
+
+function _same(arr1, arr2) {
+  if (arr1.length !== arr2.length) {
+    return false;
+  }
+  const frequencyCounter1 = {};
+  const frequencyCounter2 = {};
+
+  for (const val of arr1) {
+    frequencyCounter1[val] = (frequencyCounter1[val] || 0) + 1;
+  }
+
+  for (const val of arr2) {
+    frequencyCounter2[val] = (frequencyCounter2[val] || 0) + 1;
+  }
+
+  for (const key in frequencyCounter1) {
+    if (!(key ** 2 in frequencyCounter2)) {
+      return false;
+    }
+    if (frequencyCounter2[key ** 2] !== frequencyCounter1[key]) {
+      return flase;
+    }
+  }
+  return true;
+}
