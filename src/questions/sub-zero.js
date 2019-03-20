2824e6a5b00b312ee7bf5df27f97f58f81cb0162
Paul Tobin <tobster111@gmail.com>
2019-03-20 12:25:51 +0000

    Added a function for counting the unique values in an array

--- src/questions/sub-zero.js
+++ src/questions/sub-zero.js
@@ -2,6 +2,10 @@
 // Time Complexity - O(N^2)
 // Space Complexity - O(1)
 
+// Write a function called sumZero which accepts a sorted array of intergers
+// The function should find the first pair where the sum is 0. Return an array
+// that includes both values that sum to zero or undefined if a pair does not exist
+
 function sumZero(arr) {
   for (let i = 0; i < arr.length; i++) {
     for (let j = i + 1; j < arr.length; j++) {
