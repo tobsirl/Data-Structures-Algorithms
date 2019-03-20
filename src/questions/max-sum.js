60edbc8caf6f5f9f8e8046c4e858ce9a88c2ecdb
Paul Tobin <tobster111@gmail.com>
2019-03-20 15:47:45 +0000

    Added items in an array using 2 nested loops

---
+++ src/questions/max-sum.js
@@ -0,0 +1,21 @@
+// Write a function called maxSubarraySum which accepts and array of integers
+// and number called n. The function should calculate the maximum of n consecutive
+// elements in the array
+
+// using two nested for loops O(N^2)
+function maxSubarraySum(arr, num) {
+  if (num > arr.length) {
+    return null;
+  }
+  let max = -Infinity;
+  for (let i = 0; i < arr.length - num + 1; i++) {
+    let temp = 0;
+    for (let j = 0; j < num; j++) {
+      temp += arr[i + j];
+    }
+    if (temp > max) {
+      max = temp;
+    }
+  }
+  return max;
+}
