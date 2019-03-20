23a8a3e938fe5ae65ca5123e9dcab4d6de0d75a4
Paul Tobin <tobster111@gmail.com>
2019-03-20 11:32:54 +0000

    Added two functions for zero-sum, one using nested for loops and one using a while loop

---
+++ src/questions/sub-zero.js
@@ -0,0 +1,34 @@
+// Multiple pointers patterns
+// Time Complexity - O(N^2)
+// Space Complexity - O(1)
+
+function sumZero(arr) {
+  for (let i = 0; i < arr.length; i++) {
+    for (let j = i + 1; j < arr.length; j++) {
+      if (arr[i] + arr[j] === 0) {
+        return [arr[i], arr[j]];
+      }
+    }
+  }
+}
+
+sumZero([-4, -3, -2, -1, 0, 1, 2, 5]);
+
+// Time Complexity - O(N)
+// Space Complexity - O(1)
+
+function _sumZero(arr) {
+  let left = 0;
+  let right = arr.length - 1;
+  while (left < right) {
+    const sum = arr[left] + arr[right];
+    if (sum === 0) {
+      return [arr[left]], arr[right];
+    }
+    if (sum > 0) {
+      right--;
+    } else {
+      left++;
+    }
+  }
+}
