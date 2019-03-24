671d0c80eb57a9603637b3bf383cebf5a698071e
Paul Tobin <tobster111@gmail.com>
2019-03-24 12:46:01 +0000

    Added Naive String Search with nested loops

---
+++ src/algorithms/NaiveStringSearch.js
@@ -0,0 +1,18 @@
+// Pseudocode
+// 	• Loop over the longer string
+// 	• Loop over the shorter string
+// 	• If the characters don't match, break out of the inner loop
+// 	• If the characters do match, keep going
+// 	• If you complete the inner loop and find a match, increment the count of matches
+// 	• Return the count
+
+function naiveSearch(long, short) {
+  let count = 0;
+  for (let i = 0; i < long.length; i++) {
+    for (let j = 0; j < short.length; j++) {
+      if (short[j] !== long[i + j]) break;
+      if (j === short.length - 1) count++;
+    }
+  }
+  return count;
+}
