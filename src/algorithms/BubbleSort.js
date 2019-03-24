6445c33adf4412da751456865f5bc7677c79e671
Paul Tobin <tobster111@gmail.com>
2019-03-24 16:38:28 +0000

    Added an unoptimized version of bubblesort

---
+++ src/algorithms/BubbleSort.js
@@ -0,0 +1,36 @@
+// !Bubble Sort Pseudocode
+// 	• Start looping from with a variable called i the end of the array towards the beginning
+// 	• Start an inner loop with a variable called j from the beginning until i - 1
+// 	• If arr[j] is greater than arr[j + 1], swap those two values!
+// 	• Return the sorted array
+
+// UNOPTIMIZED VERSION OF BUBBLE SORT
+function bubbleSort(arr) {
+  for (let i = arr.length; i > 0; i--) {
+    for (let j = 0; j < i - 1; j++) {
+      console.log(arr, arr[j], arr[j + 1]);
+      if (arr[j] > arr[j + 1]) {
+        const temp = arr[j];
+        arr[j] = arr[j + 1];
+        arr[j + 1] = temp;
+      }
+    }
+  }
+  return arr;
+}
+
+// ES2015 Version
+function bubbleSort(arr) {
+  const swap = (arr, idx1, idx2) => {
+    [arr[idx1], arr[idx2]] = [arr[idx2], arr[idx1]];
+  };
+
+  for (let i = arr.length; i > 0; i--) {
+    for (let j = 0; j < i - 1; j++) {
+      if (arr[j] > arr[j + 1]) {
+        swap(arr, j, j + 1);
+      }
+    }
+  }
+  return arr;
+}
