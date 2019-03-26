8d2b9f0913cc50cf97c8966450596b1c150aee2c
Paul Tobin <tobster111@gmail.com>
2019-03-26 12:01:51 +0000

    Merge Sort - Recursive Merge Sort

--- src/algorithms/MergeSort.js
+++ src/algorithms/MergeSort.js
@@ -32,3 +32,19 @@ function merge(arr1, arr2) {
   }
   return results;
 }
+
+
+// ! Merge Sort Pseudocode
+// 	• Break up the array into halves until you have arrays that are empty or have one element
+// 	• Once you have smaller sorted arrays, merge those arrays with other sorted arrays until you are back at the full length of the array
+// 	• Once the array has been merged back together, return the merged (and sorted!) array
+
+
+// Recrusive Merge Sort
+function mergeSort(arr) {
+  if (arr.length <= 1) return arr;
+  const mid = Math.floor(arr.length / 2);
+  const left = mergeSort(arr.slice(0, mid));
+  const right = mergeSort(arr.slice(mid));
+  return merge(left, right);
+}
