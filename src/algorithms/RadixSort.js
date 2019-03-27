a96f8e16513e4b2e2890e47973ff9dc245ef2a9f
Paul Tobin <tobster111@gmail.com>
2019-03-27 08:55:04 +0000

    Radix Sort - Added helper functions - getDigit - digitCount - mostDigits added descriptions

--- src/algorithms/RadixSort.js
+++ src/algorithms/RadixSort.js
@@ -1,12 +1,16 @@
+// returns the digit in num at the given place value
 function getDigit(num, i) {
   return Math.floor(Math.abs(num) / Math.pow(10, i)) % 10;
 }
 
+// returns the number of digits in num
 function digitCount(num) {
   if (num === 0) return 1;
   return Math.floor(Math.log10(Math.abs(num))) + 1;
 }
 
+// given an array of numbers, returns the number of digits 
+// in the largest number in the list
 function mostDigits(nums) {
   let maxDigits = 0;
   for (let i = 0; i < nums.length; i++) {
@@ -15,4 +19,3 @@ function mostDigits(nums) {
   return maxDigits;
 }
 
-mostDigits([23, 567, 89, 12234324, 90]);
