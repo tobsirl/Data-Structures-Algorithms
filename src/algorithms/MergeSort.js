2cc8e3a6e98b81f6ad5bd41cb927f2ec0243d962
Paul Tobin <tobster111@gmail.com>
2019-04-14 17:55:57 +0100

    Added DoublyLinkedList with a push method

--- src/algorithms/MergeSort.js
+++ src/algorithms/MergeSort.js
@@ -33,12 +33,10 @@ function merge(arr1, arr2) {
   return results;
 }
 
-
 // ! Merge Sort Pseudocode
 // 	• Break up the array into halves until you have arrays that are empty or have one element
 // 	• Once you have smaller sorted arrays, merge those arrays with other sorted arrays until you are back at the full length of the array
 // 	• Once the array has been merged back together, return the merged (and sorted!) array
-
 
 // Recrusive Merge Sort
 function mergeSort(arr) {
---
+++ src/data-structures/DoublyLinkedList.js
@@ -0,0 +1,33 @@
+class Node {
+  constructor(value) {
+    this.value = value;
+    this.next = null;
+    this.prev = null;
+  }
+}
+
+class DoublyLinkedList {
+  constructor() {
+    this.head = null;
+    this.tail = null;
+    this.length = 0;
+  }
+
+  push(value) {
+    const newNode = new Node(value);
+    if (this.length === 0) {
+      this.head = newNode;
+      this.tail = newNode;
+    } else {
+      this.tail.next = newNode;
+      newNode.prev = this.tail;
+      this.tail = newNode;
+    }
+    this.length++;
+    return this;
+  }
+}
+
+const LinkedList = new DoublyLinkedList();
+
+LinkedList.push('One');
