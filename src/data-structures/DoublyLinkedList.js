bc0488bce7b3df0242a90c17a4cdd247709aaa75
Paul Tobin <tobster111@gmail.com>
2019-04-15 10:13:47 +0100

    Added the shift method to remove a node from the beginning of the linked list

--- src/data-structures/DoublyLinkedList.js
+++ src/data-structures/DoublyLinkedList.js
@@ -14,31 +14,48 @@ class DoublyLinkedList {
   }
 
   push(value) {
-    const newNode = new Node(value);
+    const newNode = new Node(value); // create the new node with the value passed in
     if (this.length === 0) {
-      this.head = newNode;
+      // check that the list isn't empty, checking length
+      this.head = newNode; // point the head + tail at the new node
       this.tail = newNode;
     } else {
-      this.tail.next = newNode;
-      newNode.prev = this.tail;
-      this.tail = newNode;
+      this.tail.next = newNode; // point the tail at the newNode
+      newNode.prev = this.tail; // point the newNode at the previous
+      this.tail = newNode; // make the newNode the tail
     }
-    this.length++;
-    return this;
+    this.length++; // increment the length
+    return this; // return the doubly linked list
   }
 
   pop() {
-    if (!this.head) return undefined;
-    const temp = this.tail;
+    if (!this.head) return undefined; // check if the list is empty
+    const temp = this.tail; // create a temp variable to store the vaule of the node being removed
     if (this.length === 1) {
+      // if the length is 1, make the head and tail null
       this.head = null;
-      this.head = null;
+      this.tail = null;
     } else {
-      this.tail = temp.prev;
-      this.tail.next = null;
+      this.tail = temp.prev; // make the previous node the new tail
+      this.tail.next = null; // remove the pointers
       temp.prev = null;
     }
-    this.length--;
+    this.length--; // decrement the list length
+    return temp; // return the removed node
+  }
+
+  shift() {
+    if (this.length === 0) return undefined; // check if the list is empty
+    const temp = this.head; // create a temp variable to store the current head
+    if (this.length === 1) {
+      this.head = null;
+      this.tail = null;
+    }
+    this.head = temp.next; // sever the pointers to the old node
+    this.head.prev = null;
+    temp.next = null;
+
+    this.length--; // decrement the list length
     return temp;
   }
 }
