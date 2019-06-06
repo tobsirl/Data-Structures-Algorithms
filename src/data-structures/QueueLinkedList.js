56aa7386f623cdd9d481174712af38fdf043b70d
Paul Tobin <tobster111@gmail.com>
2019-06-06 13:30:02 +0100

    Added a Queue using a linked list

---
+++ src/data-structures/QueueLinkedList.js
@@ -0,0 +1,14 @@
+class Node {
+  constructor() {
+    this.value = value;
+    this.next = null;
+  }
+}
+
+class Queue {
+  constructor() {
+    this.first = null;
+    this.last = null;
+    this.size = 0;
+  }
+}
