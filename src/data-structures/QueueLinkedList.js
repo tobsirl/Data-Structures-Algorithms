4e324caa29d34b704283c7d33f79b4315ee54f23
Paul Tobin <tobster111@gmail.com>
2019-06-06 14:11:12 +0100

    Implemented the enqueue method for adding a new node to the beginning of the queue

--- src/data-structures/QueueLinkedList.js
+++ src/data-structures/QueueLinkedList.js
@@ -4,11 +4,26 @@ class Node {
     this.next = null;
   }
 }
-
 class Queue {
   constructor() {
     this.first = null;
     this.last = null;
     this.size = 0;
   }
+
+  enqueue(value) {
+    const newNode = new Node(value); // create a new node
+    if (!this.first) {
+      // check that the queue isn't empty
+      this.first = newNode; // assign first to the new node
+      this.last = newNode; // assign last to the new node
+    } else {
+      this.last.next = newNode; // attach the pointer to the new node
+      this.last = newNode; // assign the value to the new node
+    }
+    this.size++; // increment the size
+    return this.size; // return the size
+  }
+
+  dequeue() {}
 }
