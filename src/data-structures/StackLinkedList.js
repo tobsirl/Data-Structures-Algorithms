57cd7855b942d82ee70a6254dd115a8afb581d0d
Paul Tobin <tobster111@gmail.com>
2019-06-06 12:30:48 +0100

    Implemented the pop method for removing a node to the top of the stack

--- src/data-structures/StackLinkedList.js
+++ src/data-structures/StackLinkedList.js
@@ -12,17 +12,31 @@ class Stack {
     this.size = 0;
   }
 
-  pop(value) {
-    const newNode = new Node(value);
+  push(value) {
+    // takes a value
+    const newNode = new Node(value); // create a new node
     if (!this.first) {
-      this.first = newNode;
-      this.last = newNode;
+      // check that the stack isn't empty
+      this.first = newNode; // set this.first to newNode
+      this.last = newNode; // set this.last to newNode
     } else {
-      const temp = this.first;
-      this.first = newNode;
-      this.first.next = temp;
+      const temp = this.first; // create a temp variable
+      this.first = newNode; // assign this.first to the newNode
+      this.first.next = temp; // set the old node to the temp varialbe
     }
-    this.size++;
-    return this.size;
+    this.size++; // increment the size
+    return this.size; // return the size
+  }
+
+  pop() {
+    if (!this.first) return null; // return null if the stack is empty
+    const tempFirst = this.first; // create a temp variable to store this.first
+    if (this.first === this.last) {
+      // check for only one node
+      this.last = null; // reset this.last to null
+    }
+    this.first = this.first.next; // update first
+    this.size--; // decrement the size
+    return tempFirst.value; // return the value in tempFirst
   }
 }
