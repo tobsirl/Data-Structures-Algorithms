cade4f9daf7c87f1f2bdcbad38b07190b9b310d3
Paul Tobin <tobster111@gmail.com>
2019-06-21 11:57:05 +0100

    Depth First Search InOrder

--- src/data-structures/Tree Traversal/BreadthFirstTree.js
+++ src/data-structures/Tree Traversal/BreadthFirstTree.js
@@ -92,4 +92,16 @@ class BinarySearchTree {
     traverse(this.root); // call traverse with the current node
     return data; // return the data array
   }
+
+  DFSInOrder() {
+    const data = []; // create an array to store the data
+    function traverse(node) {
+      // helper function to traverse a tree
+      if (node.left) traverse(node.left); // check for a left property, call traverse with the left node
+      data.push(node.value); // push the value into data
+      if (node.right) traverse(node.right); // check for a right property, call traverse with the right node
+    }
+    traverse(this.root); // call traverse with the current node
+    return data; // return the data array
+  }
 }
