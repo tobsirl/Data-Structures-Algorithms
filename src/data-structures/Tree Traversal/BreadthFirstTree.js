5b8f4fd43e93da2cd764f298b4fb0930c3ac431e
Paul Tobin <tobster111@gmail.com>
2019-06-21 11:18:19 +0100

    Depth First Search PostOrder

--- src/data-structures/Tree Traversal/BreadthFirstTree.js
+++ src/data-structures/Tree Traversal/BreadthFirstTree.js
@@ -80,4 +80,16 @@ class BinarySearchTree {
     traverse(this.root); // call traverse with the current node
     return data; // return the data array
   }
+
+  DFSPostOrder() {
+    const data = []; // create an array to store the data
+    function traverse(node) {
+      // helper function to traverse a tree
+      if (node.left) traverse(node.left); // check for a left property, call traverse with the left node
+      if (node.right) traverse(node.right); // check for a right property, call traverse with the right node
+      data.push(node.value); // push the value into data
+    }
+    traverse(this.root); // call traverse with the current node
+    return data; // return the data array
+  }
 }
